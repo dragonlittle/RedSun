c1b300b11e3f38d4a771c92020f745c51b9cc0fe
dragonlittle <theelderkid@gmail.com>
2013-09-28 18:20:02 +0700

    update

--- Module/include/database_connect.php
+++ Module/include/database_connect.php
@@ -1,9 +1,10 @@
 <?php
-	$dbc = new mysqli('localhost','root','root','redsun');
+	$dbc = new mysqli('localhost','root','','redsun');
 	if($dbc->connect_error){
 		die($dbc->connect_error);
 	}
 	else{
 		$dbc->query("set names utf8");
+        //echo "Kết nối thành công";
 	}
 ?>
